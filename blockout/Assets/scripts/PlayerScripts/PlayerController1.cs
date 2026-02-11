b8f03fc57689b45572f06604334a4ee0bd65d0aa
aneiwert <a.neiwert1@saints.mhcc.edu>
2026-02-11 01:46:11 -0800

    last commit lol

--- blockout/Assets/scripts/PlayerScripts/PlayerController1.cs
+++ blockout/Assets/scripts/PlayerScripts/PlayerController1.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Public float meant to control base player move speed and Interact Mechanic
-    public float speed = 5f;
+    public float MoveSpeed = 10f;
+    public float RotSpeed = 75f; 
     // Private floats meant to control variables within script
-    private float translation;
-    private float straffe;
+    private float vInput;
+    private float hInput;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,9 +22,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        translation = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        straffe = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.Translate(straffe, 0, translation);
+        vInput = Input.GetAxis("Vertical") * MoveSpeed; 
+        hInput = Input.GetAxis("Horizontal") * RotSpeed;
+       
+        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime); 
+
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime); 
+ 
 
         if (Input.GetKeyDown("escape")) {
             Cursor.lockState = CursorLockMode.None;
